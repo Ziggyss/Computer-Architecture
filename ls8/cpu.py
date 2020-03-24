6e79788f26d996c316f0eb917e0178b74e6d12ea
Lisa Wilton <mishmao14@gmail.com>
2020-03-24 15:59:02 +0000

    add to the load method

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -19,14 +19,14 @@ class CPU:
 
     def ram_read(self, MAR):
             # return the value at the memory address 
-            return self.ram[MAR]
+            return self.ram[int(MAR, 2)]
 
     # MAR is the Memory Address Register (the address)
     # MDR is the Memory Data Register (the value)   
 
     def ram_write(self, MDR, MAR):
         # assign the value to the memory address location
-        self.ram[MAR] = MDR
+       self.ram[int(MAR), 2] = MDR
 
 
     def load(self, program):
@@ -48,9 +48,33 @@ class CPU:
 
         # program = sys.argv[1]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+        if len(sys.argv) != 2:
+            print("usage: ls8.py filename")
+            sys.exit(1)
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    # split line before and after comment symbol
+                    comment_split = line.split("#")
+
+                    # extract our number
+                    num = comment_split[0].strip() # trim whitespace
+
+                    if num == '':
+                        continue # ignore blank lines
+
+                    # convert our binary string to a number
+                    x = int(num, 2)
+
+                    # print the x in bin and dec
+                    print(f"{x:08b}: {x:d}")
+
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)    
 
 
     def alu(self, op, reg_a, reg_b):
@@ -90,14 +114,14 @@ class CPU:
 
             if cmd == LDI:
                 # read the values at the next two positions
-                operand_a = self.ram_read(self.pc + 1)
-                operand_b = self.ram_read(self.pc + 2)
+                operand_a = int(self.ram[self.pc + 1], 2)
+                operand_b = int(self.ram[self.pc + 1], 2)
                 self.register[operand_a] = operand_b
                 # bypass the three positions that have just been used to move to the next
                 self.pc += 3
 
             elif cmd == PRN:
-                register_index = self.ram_read(self.pc + 1)
+                register_index = int(self.ram[self.pc + 1], 2)
                 value = self.register[register_index]
                 print(value)
                 self.pc += 2
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -6,19 +6,21 @@ import sys
 
 from cpu import *
 
-# program = sys.argv[1]
-program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+program = sys.argv[1]
+# program = [
+#             # From print8.ls8
+#             0b10000010, # LDI R0,8
+#             0b00000000,
+#             0b00001000,
+#             0b01000111, # PRN R0
+#             0b00000000,
+#             0b00000001, # HLT
+#         ]
+
 
 cpu = CPU()
 
 cpu.load(program)
 
 cpu.run()
+
