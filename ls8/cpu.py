672655c40040dd487097a20379218f4b3e6586f3
Lisa Wilton <mishmao14@gmail.com>
2020-03-25 09:58:41 +0000

    multiplier fixed

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,6 +5,7 @@ import sys
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
+MUL = 0b10100010
 
 
 
@@ -48,43 +49,43 @@ class CPU:
 
         # program = sys.argv[1]
 
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
+        for instruction in program:
+            self.ram[address] = instruction
+            address += 1
 
-        if len(sys.argv) != 2:
-            print("usage: ls8.py filename")
-            sys.exit(1)
-        try:
-            with open(sys.argv[1]) as f:
-                for line in f:
-                    # split line before and after comment symbol
-                    comment_split = line.split("#")
+        # if len(sys.argv) != 2:
+        #     print("usage: ls8.py filename")
+        #     sys.exit(1)
+        # try:
+        #     with open(sys.argv[1]) as f:
+        #         for line in f:
+        #             # split line before and after comment symbol
+        #             comment_split = line.split("#")
 
-                    # extract our number
-                    num = comment_split[0].strip() # trim whitespace
+        #             # extract our number
+        #             num = comment_split[0].strip() # trim whitespace
 
-                    if num == '':
-                        continue # ignore blank lines
+        #             if num == '':
+        #                 continue # ignore blank lines
 
-                    # convert our binary string to a number
-                    x = int(num, 2)
+        #             # convert our binary string to a number
+        #             x = int(num, 2)
 
-                    # print the x in bin and dec
-                    print(f"{x:08b}: {x:d}")
+        #             # print the x in bin and dec
+        #             print(f"{x:08b}: {x:d}")
 
-        except FileNotFoundError:
-            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
-            sys.exit(2)    
+        # except FileNotFoundError:
+        #     print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+        #     sys.exit(2)    
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         if op == "MUL":
-           self.reg[reg_a] *= self.reg[reg_b]
+           self.register[reg_a] *= self.register[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -117,7 +118,7 @@ class CPU:
             if cmd == LDI:
                 # read the values at the next two positions
                 operand_a = self.ram[self.pc + 1]
-                operand_b = self.ram[self.pc + 1]
+                operand_b = self.ram[self.pc + 2]
                 self.register[operand_a] = operand_b
                 # bypass the three positions that have just been used to move to the next
                 self.pc += 3
@@ -127,6 +128,12 @@ class CPU:
                 value = self.register[register_index]
                 print(value)
                 self.pc += 2
+
+            elif cmd == MUL:
+                operand_a = self.ram[self.pc + 1]
+                operand_b = self.ram[self.pc + 2]
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3    
 
               
             elif cmd == HLT:
